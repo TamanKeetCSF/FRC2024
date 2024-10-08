7e309a665e8538d9777d8fe2cdb1fbe803374ef1
notBers <109546794+notBers@users.noreply.github.com>
2024-10-08 11:01:08 -0600

    v0.0.1

--- src/main/java/frc/robot/commands/DriveTrain_TankDrive.java
+++ src/main/java/frc/robot/commands/DriveTrain_TankDrive.java
@@ -1,32 +1,51 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.XboxController;
 
-public class DriveTrain_TankDrive extends Command {
-  /** Creates a new DriveTrain_TankDrive. */
-  public DriveTrain_TankDrive() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+/**
+ * Command for controlling the robot drive system in tank drive mode using an Xbox controller.
+ */
+public class DriveTrain_TankDrive extends Command{
+    private final DriveTrain m_driveTrain;
+    private final XboxController controller;
+    private final double deadzone = 0.07;
+    private final double speedScale = 1.0;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    public DriveTrain_TankDrive(DriveTrain subsystem, XboxController controller) {
+        m_driveTrain = subsystem;
+        this.controller = controller;
+        addRequirements(m_driveTrain);  // Declare subsystem dependencies
+    }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+    @Override
+    public void initialize() {
+        m_driveTrain.setMotors(0, 0);  // Stop motors on initialization
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    @Override
+    public void execute() {
+        // Get joystick values for tank drive
+        double leftPower = controller.getLeftY() * speedScale;
+        double rightPower = controller.getRightY() * speedScale;
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+        // Apply deadzone to avoid accidental movement
+        leftPower = (Math.abs(leftPower) < deadzone) ? 0 : leftPower;
+        rightPower = (Math.abs(rightPower) < deadzone) ? 0 : rightPower;
+
+        // Set the motors with scaled power
+        m_driveTrain.setMotors(leftPower, rightPower);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_driveTrain.setMotors(0, 0);  // Stop motors when the command ends
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;  // Run until interrupted
+    }
 }
+
