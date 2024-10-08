4dfdcf2adf11c1a0dba478b24b7e871b73b5b44a
notBers <109546794+notBers@users.noreply.github.com>
2024-10-08 10:51:45 -0600

    demo v0.0.1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,63 +1,86 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.*;
+import frc.robot.commands.DriveTrain_TankDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    private static RobotContainer m_robotContainer = new RobotContainer();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
+    // Subsystems
+    public final intake m_intake = new intake();
+    public final outake m_outake = new outake();
+    public final DriveTrain m_driveTrain = new DriveTrain();  // DriveTrain subsystem
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    // Joysticks
+    private final XboxController xboxController1 = new XboxController(0);
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
+    // A chooser for autonomous commands
+    SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    private RobotContainer() {
+        // Configure the button bindings
+        configureButtonBindings();
+
+        // Set default drive command to tank drive using the joysticks
+        m_driveTrain.setDefaultCommand(new DriveTrain_TankDrive(m_driveTrain, xboxController1));
+
+        // Configure autonomous sendable chooser
+        SmartDashboard.putData("Auto Mode", m_chooser);
+    }
+
+    public static RobotContainer getInstance() {
+        return m_robotContainer;
+    }
+
+    /**
+     * Use this method to define your button->command mappings.
+     */
+    private void configureButtonBindings() {
+        // Intake button mappings
+        final JoystickButton buttonA = new JoystickButton(xboxController1, XboxController.Button.kA.value);
+        final JoystickButton buttonB = new JoystickButton(xboxController1, XboxController.Button.kB.value);
+
+        // Outake button mappings
+        final JoystickButton buttonX = new JoystickButton(xboxController1, XboxController.Button.kX.value);
+        final JoystickButton buttonY = new JoystickButton(xboxController1, XboxController.Button.kY.value);
+
+        // Intake control
+        buttonA.onTrue(new InstantCommand(() -> m_intake.activateIntake(), m_intake)
+                        .withInterruptBehavior(InterruptionBehavior.kCancelSelf));
+        buttonB.onTrue(new InstantCommand(() -> m_intake.stopIntake(), m_intake));
+
+        // Outake control
+        buttonX.onTrue(new InstantCommand(() -> m_outake.outakeAmp(), m_outake)
+                        .withInterruptBehavior(InterruptionBehavior.kCancelSelf));
+        buttonY.onTrue(new InstantCommand(() -> m_outake.stopOutake(), m_outake));
+    }
+
+    public XboxController getXboxController1() {
+        return xboxController1;
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // The selected command will be run in autonomous
+        return m_chooser.getSelected();
+    }
 }
+
